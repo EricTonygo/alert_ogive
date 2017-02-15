589d4047f83ed5be134e33290dde2f589e956887
EricTonygo <tonye.eric@gmail.com>
2017-02-15 18:47:13 +0100

    mise à jour du nom du numero

--- src/OGIVE/AlertBundle/Controller/SubscriberController.php
+++ src/OGIVE/AlertBundle/Controller/SubscriberController.php
@@ -238,7 +238,7 @@ class SubscriberController extends Controller {
         $twilio = $this->get('twilio.api');
         //$messages = $twilio->account->messages->read();
         $message = $twilio->account->messages->sendMessage(
-                'MG8e369c4e5ea49ce989834c5355a1f02f', // From a Twilio number in your account
+                'OGIVE INFOS', // From a Twilio number in your account
                 $subscriber->getPhoneNumber(), // Text any number
                 $content
         );
