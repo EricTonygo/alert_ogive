3df2450f568ee0a9aaeddb8ff3ef10fa4b83f37c
EricTonygo <tonye.eric@gmail.com>
2017-07-23 00:52:35 +0200

    Ajout de la mention OGIVE SOLUTIONS pour les messages de Mballa

--- src/OGIVE/AlertBundle/Services/SmsService.php
+++ src/OGIVE/AlertBundle/Services/SmsService.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace OGIVE\AlertBundle\Services;
+
 use Twilio\Rest\Client;
+
 /**
  * Description of MailService
  *
@@ -10,8 +12,8 @@ use Twilio\Rest\Client;
 class SmsService {
 
     protected $twilio_client;
-    protected  $username = "ACdda206efadd5de15c5f0121a6a982a9c";
-    protected  $password = "e77b1a26567145aa789a3dc0d701dea0";
+    protected $username = "ACdda206efadd5de15c5f0121a6a982a9c";
+    protected $password = "e77b1a26567145aa789a3dc0d701dea0";
 
     public function __construct() {
         $this->twilio_client = new Client($this->username, $this->password);
@@ -19,8 +21,12 @@ class SmsService {
 
     public function sendSms($phoneNumber, $message) {
         if ($phoneNumber != "") {
-            if($phoneNumber == "+237671034458" || $phoneNumber == "+237670034454" || $phoneNumber == "+237671034453" || $phoneNumber == "+237671034445"){
-                $message += " OGIVE SOLUTIONS. Tel: 243803895/694200310";
+            if ($phoneNumber == "+237671034458" || $phoneNumber == "+237670034454" || $phoneNumber == "+237671034453" || $phoneNumber == "+237671034445") {
+                if (substr(trim($message), -1) === ".") {
+                    $message += " OGIVE SOLUTIONS. Tel: 243803895/694200310";
+                }else{
+                    $message += " . OGIVE SOLUTIONS. Tel: 243803895/694200310";
+                }                
             }
             $this->twilio_client->messages->create(
                     $phoneNumber, // Text any number
