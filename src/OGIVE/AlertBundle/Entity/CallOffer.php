30f37a3669309f6e443abd3cdee1d11ac00218cc
EricTonygo <tonye.eric@gmail.com>
2017-10-30 13:00:02 +0100

    New update for Osms api

--- src/OGIVE/AlertBundle/Entity/CallOffer.php
+++ src/OGIVE/AlertBundle/Entity/CallOffer.php
@@ -68,7 +68,7 @@ class CallOffer extends AlertProcedure {
             }
         }
         //$abstract = $this->getReference() . " du " . date("d/m/Y", strtotime($this->getPublicationDate())) . " lance par " . $this->getOwner() . " pour " . $object_abstract . $dot . " Depot des offres le " . date("d/m/Y", strtotime($this->getOpeningDate())) . " a " . date("H:i", strtotime($this->getOpeningDate())) . '.';
-        $abstract = $this->getReference() . " du " . date("d/m/Y", strtotime($this->getPublicationDate())) . " lance par " . $this->getOwner()." Depot des offres le " . date("d/m/Y", strtotime($this->getOpeningDate())) . " a " . date("H:i", strtotime($this->getOpeningDate())) . '. Detail par email ou à ' . $this->getUrlDetails();
+        $abstract = $this->getReference() . " du " . date("d/m/Y", strtotime($this->getPublicationDate())) . " lance par " . $this->getOwner().". Depot des offres le " . date("d/m/Y", strtotime($this->getOpeningDate())) . " a " . date("H:i", strtotime($this->getOpeningDate())) . '. Detail par email ou à ' . $this->getUrlDetails();
         return $abstract;
     }
 
